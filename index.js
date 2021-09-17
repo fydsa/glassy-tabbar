f6bcf9afde5f8a43958ca35337995fbce61c8977
Saba <saba.alikhani@ymail.com>
2021-09-17 15:40:44 +0430

    Changed the logic of the indicator bg color into the basis of the active tab number

--- index.js
+++ index.js
@@ -11,19 +11,16 @@ indicator.style.backgroundColor = "#e2921a";
 function clickTab() {
   let active_tab = document.querySelector("input:checked + label");
   indicator.style.left = `${active_tab.offsetLeft + 5}px`;
-  let indicator_left = indicator.style.left;
+  let active_tab_number = active_tab.getAttribute("for");
   switch (true) {
-    case indicator_left === "17px" || indicator_left === "18px":
+    case active_tab_number === "tab-one":
       indicator.style.backgroundColor = "#e2921a";
       break;
-    case indicator_left === "91px" || indicator_left === "92px":
+    case active_tab_number === "tab-two" || active_tab_number === "tab-three":
       indicator.style.backgroundColor = "#e29818";
       break;
-    case indicator_left === "166px" || indicator_left === "167px":
-      indicator.style.backgroundColor = "#e29818";
-      break;
-    case indicator_left === "241px" || indicator_left === "242px":
-      indicator.style.backgroundColor = "#e19c16";
+    case active_tab_number === "tab-four":
+      indicator.style.backgroundColor = "#e09916";
       break;
     default:
       indicator.style.backgroundColor = "#e2921a";
